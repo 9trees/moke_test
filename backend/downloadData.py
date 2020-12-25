05691e3cb3d676c8e39f4389d9d800507ba4d6b5
sivashankar <passwardstrength@gmail.com>
2020-12-25 17:35:35 +0000

    working with Mathjax equation
    works well, still img is pending

--- backend/downloadData.py
+++ backend/downloadData.py
@@ -2,10 +2,10 @@ import urllib.request
 import bs4 as bs
 
 def call_back():
-    toremove = dict.fromkeys((ord(c) for c in u'\xa0\n\t '))
+    toremove = dict.fromkeys((ord(c) for c in u'\xa0\n\t'))
     # Fetch the html file
     response = urllib.request.urlopen(
-        'https://www.studyadda.com/sample-papers/neet-sample-test-paper-83_q1/1296/403162')
+        'https://www.studyadda.com/sample-papers/neet-sample-test-paper-83_q1/1296/403163')
     html_doc = response.read()
 
     # Parse the html file
@@ -17,9 +17,14 @@ def call_back():
     for line in details_ques_ans.find_all("span"):
         #print(line.get_text(), len(line.get_text()))
         if(len(line.get_text()) >= 1):
-            print(line.contents, type(line.contents))
+            for text in line.contents:
+                try:
+                    text = text.translate(toremove)
+                    print(text)
+                except:
+                    print(text)
     answer = soup.find(class_="ans_panel")
     print(answer.get_text())
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     call_back()
